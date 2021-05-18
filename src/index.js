e200a4ec39457936145eeba3ac3ffbcd96d7aedc
akshay-narkar <akshaynarkar1295@gmail.com>
2021-05-18 21:12:47 +0530

    Call logic and dom from index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheet.css';
+import * as dom1 from './dom';
+import logic from './logic';
+
+dom1.dom();
+logic(dom1);
