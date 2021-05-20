6e18fc823970c3e8428c3d584e03919b522263ce
akshay-narkar <akshaynarkar1295@gmail.com>
2021-05-21 01:12:45 +0530

    Update logic

--- src/logic.js
+++ src/logic.js
@@ -1,63 +1,49 @@
 const submitlist = document.querySelector('#submitlist');
 const submittask = document.querySelector('#submittask');
 const listname = document.querySelector('#defaultlist');
+const listoflist1 = document.querySelectorAll('.listoflist');
 
-function logic(dom1) {
+function logic() {
+  class Createlist1 {
+    constructor(list) {
+      this.list = list;
+      this.todos = [];
+    }
+  }
 
-class Createlist1 {
+  let liststasks = [];
 
-          constructor (list) {
-            this.list = list;
-            }
-          }
+  if (!localStorage.getItem('liststore')) {
+    const listdefault = new Createlist1(listname.textContent);
+    liststasks.push(listdefault);
+    localStorage.setItem('liststore', JSON.stringify(liststasks));
+  }
 
+  function localstorage1() {
+    if (localStorage.getItem('liststore')) {
+      liststasks = JSON.parse(localStorage.liststore);
+    }
+    return liststasks;
+  }
 
+  const createlist = (e) => {
+    const liststasks = localstorage1();
+    const list1 = new Createlist1(list.value);
+    liststasks.push(list1);
+    localStorage.setItem('liststore', JSON.stringify(liststasks));
+  };
 
-let liststasks = [];
+  // function addtolist () {
 
-if(!localStorage.getItem('liststore')){
-let listdefault = new Createlist1(listname.textContent);
-liststasks.push(listdefault);
-localStorage.setItem("liststore", JSON.stringify(liststasks));
+  // let liststasks = localstorage1()
+  //     // let list = liststasks.push(x,y,z)
+  //  let tasks = ['x',1,'High',888];
+  //     // let book1 = new Createbook(book.value, author.value, pages.value, readRadios1.checked);
+  //   liststasks.push(tasks);
+  //   localStorage.setItem("liststore", JSON.stringify(liststasks));
+  // }
+
+  // submittask.addEventListener('click',addtotask)
+  submitlist.addEventListener('click', createlist);
 }
-
-
-
-function localstorage1(){
-
-      if(localStorage.getItem('liststore')) {
-         liststasks = JSON.parse(localStorage.liststore);
-        }
-  return liststasks;
-
-}
-
-
-function createlist(e){
-  
-
-      let liststasks = localstorage1();
-      let list1 = new Createlist1(list.value);
-      liststasks.push(list1);
-      localStorage.setItem("liststore", JSON.stringify(liststasks));
-}
-
-
-
-// function addtolist () {
-
-// let liststasks = localstorage1()
-//     // let list = liststasks.push(x,y,z)
-//  let tasks = ['x',1,'High',888];
-//     // let book1 = new Createbook(book.value, author.value, pages.value, readRadios1.checked);
-//   liststasks.push(tasks);
-//   localStorage.setItem("liststore", JSON.stringify(liststasks));
-// }
-        
-
-submitlist.addEventListener('click',createlist)
-// submittask.addEventListener('click',addtotask)
-
-        }
-
 export default logic;
