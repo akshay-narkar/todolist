d93a45b1954c21e8578856eb25671df606fb8c3b
akshay-narkar <akshaynarkar1295@gmail.com>
2021-05-21 01:12:29 +0530

    Update the index file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheet.css';
 import * as dom1 from './dom';
 import logic from './logic';
-
+// import * as dom2 from './dom2';
 dom1.dom();
-logic(dom1);
+logic();
+// dom2.dom();
+
+// const listoflist1 = document.querySelector('#projectlist1');
+// console.log(listoflist1);
