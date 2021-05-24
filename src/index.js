eecce113c58de74c5f0faf55ed5d915c42384ab7
akshay-narkar <akshaynarkar1295@gmail.com>
2021-05-24 19:06:26 +0530

    Send dom function to logic

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as dom1 from './dom';
 import logic from './logic';
 // import * as dom2 from './dom2';
 dom1.dom();
-logic();
+logic(dom1);
 // dom2.dom();
 
 // const listoflist1 = document.querySelector('#projectlist1');
